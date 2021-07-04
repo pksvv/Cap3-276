7f79aaae7bba8d3f5c52bd7e70fcb69f97085df0
Vipul Gaur <gaurvipul@gmail.com>
2021-07-04 20:59:22 +0530

    eda

---
+++ helper_files/test.py
@@ -0,0 +1,42 @@
+
+import joblib
+import features_extraction
+import sys
+import numpy as np
+
+#from features_extraction import LOCALHOST_PATH #DIRECTORY_NAME
+
+
+def get_prediction_from_url(test_url):
+    features_test = features_extraction.main(test_url)
+    # we now need a 2D array as a parameter to the predict function.
+    features_test = np.array(features_test).reshape((1, -1))
+
+    clf = joblib.load('/classifier/finalized_model.sav')
+
+    pred = clf.predict(features_test)
+    return int(pred[0])
+
+
+def main():
+    url = sys.argv[1]
+
+    prediction = get_prediction_from_url(url)
+
+    # Print the probability of prediction (if needed)
+    # prob = clf.predict_proba(features_test)
+    # print 'Features=', features_test, 'The predicted probability is - ', prob, 'The predicted label is - ', pred
+    #    print "The probability of this site being a phishing website is ", features_test[0]*100, "%"
+
+    if prediction == 1:
+        # print "The website is safe to browse"
+        print("SAFE")
+    elif prediction == -1:
+        # print "The website has phishing features. DO NOT VISIT!"
+        print("PHISHING")
+
+        # print 'Error -', features_test
+
+
+if __name__ == "__main__":
+    main()
